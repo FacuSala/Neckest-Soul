34869120fc92d5eb6045cbb0280a35bde0cb6a4f
Facundo <facundom.salazar@gmail.com>
2020-07-31 19:06:32 -0300

    Limitación velocidad

--- Assets/Scripts/Prota.cs
+++ Assets/Scripts/Prota.cs
@@ -21,5 +21,17 @@ public class Prota : MonoBehaviour {
         y = Input.GetAxis("Vertical");
         rigidBody.AddForce(Vector2.right * x * velocity);
         rigidBody.AddForce(Vector2.up * y * velocity);
+
+        if (x == 0f)
+        {
+            rigidBody.AddForce(Vector2.left * 0);
+        }
+        if (y == 0f)
+        {
+            rigidBody.AddForce(Vector2.up * 0);
+        }
+        float velMaxX = Mathf.Clamp (rigidBody.velocity.x , - velocity , velocity);
+        float velMaxY = Mathf.Clamp (rigidBody.velocity.y , - velocity , velocity);
+        rigidBody.velocity = new Vector2 (velMaxX , velMaxY);
     }
 }

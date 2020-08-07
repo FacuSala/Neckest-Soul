4bbcdfc307ea933d78ed76fae08ba62ff1bc65b9
Nico <nmanograsso@gmail.com>
2020-08-07 19:04:08 -0300

    Fix no velocity

--- Assets/Scripts/Prota.cs
+++ Assets/Scripts/Prota.cs
@@ -13,25 +13,16 @@ public class Prota : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float x;
-        float y;
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
+    void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        float velMaxX = Mathf.Clamp (rigidBody.velocity.x , - velocity , velocity);
+        float velMaxY = Mathf.Clamp (rigidBody.velocity.y , - velocity , velocity);
+        
         rigidBody.AddForce(Vector2.right * x * velocity);
         rigidBody.AddForce(Vector2.up * y * velocity);
 
-        if (x == 0f)
-        {
-            rigidBody.AddForce(Vector2.left * 0);
-        }
-        if (y == 0f)
-        {
-            rigidBody.AddForce(Vector2.up * 0);
-        }
-        float velMaxX = Mathf.Clamp (rigidBody.velocity.x , - velocity , velocity);
-        float velMaxY = Mathf.Clamp (rigidBody.velocity.y , - velocity , velocity);
-        rigidBody.velocity = new Vector2 (velMaxX , velMaxY);
+        rigidBody.velocity = new Vector2 ( x==0f? 0f : velMaxX , y==0 ? 0f : velMaxY);
     }
 }

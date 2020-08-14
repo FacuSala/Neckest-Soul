975c003c4bf00580278fa7fe79158e92b63dd9fc
Nico <nmanograsso@gmail.com>
2020-08-13 23:21:16 -0300

    Make all animations to Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,41 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+    private const string lastHorizontal = "LastHorizontal";
+    private const string lastVertical = "LastVertical";
+
+    private Animator animator;
+    private Rigidbody2D rigidBody;
+    private Vector2 lastMove;
 
     public float velocity = 10f;
     public GameObject sword;
 
-    private Animator animator;
-    private Rigidbody2D rigidBody;
-    // Start is called before the first frame update
+
     void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        lastMove = Vector2.zero;
     }
 
-    // Update is called once per frame
     void Update() {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        setAnimation(x, y);
+        float x = Input.GetAxis(horizontal);
+        float y = Input.GetAxis(vertical);
 
         setMovement(x, y);
+        setAnimation(x, y);
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             sword.GetComponent<BoxCollider2D>().enabled = false;
         }
-    }
-
-    void setAnimation(float x, float y) {
-        animator.SetFloat("Horizontal", x);
-        animator.SetFloat("Vertical", y);
-
-        if (x > 0.1f)
-			transform.localScale = new Vector3(-1f, 1f, 1f);
-		if (x < -0.1f)
-			transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
     void setMovement(float x, float y) {
@@ -48,5 +44,15 @@ public class PlayerController : MonoBehaviour {
         rigidBody.AddForce(Vector2.up * y * velocity);
 
         rigidBody.velocity = new Vector2 ( x==0f? 0f : velMaxX , y==0 ? 0f : velMaxY);
+    }
+
+    void setAnimation(float x, float y) {
+        if(Mathf.Abs(x) > 0.3 || Mathf.Abs(y) > 0.3)
+            lastMove = new Vector2(x,y);
+        animator.SetBool("Walking", x!=0f || y!=0);
+        animator.SetFloat(horizontal, x);
+        animator.SetFloat(vertical, y);
+        animator.SetFloat(lastHorizontal, lastMove.x);
+        animator.SetFloat(lastVertical, lastMove.y);
     }
 }

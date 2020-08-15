2ca7d1d5488a6e50eb272625a699ef71356fdcd0
Nico <nmanograsso@gmail.com>
2020-08-15 19:48:02 -0300

    Make that enemy walk

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
-    void OnCollisionEnter2D(Collision2D collisionInfo) {
-    
-    }
+    public float speed = 1f;
+    private Rigidbody2D rigidbody;
+    public float timeBetweenSteps;
+    private float timeBetweenStepsCounter;
+    public float timeToMakeStep;
+    private float timeToMakeStepCounter;
+    public Vector2 directionToMakeStep;
+    private bool isMoving;
+
+    private Animator animator;
+    private const string vertical = "Vertical";
+    private const string horizontal = "Horizontal";
+    private const string isMovingState = "isMoving";
 
     void Start() {
-    
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+
+        timeBetweenStepsCounter = timeBetweenSteps;
+        timeToMakeStepCounter = timeToMakeStep;
     }
 
     void Update() {
-        
+        if (isMoving) {
+            timeToMakeStepCounter -= Time.deltaTime;
+            rigidbody.velocity = directionToMakeStep;
+
+            if (timeToMakeStepCounter < 0) {
+                isMoving = false;
+                timeBetweenStepsCounter = timeBetweenSteps;
+                rigidbody.velocity = Vector2.zero;
+            }
+        } else {
+            timeBetweenStepsCounter -= Time.deltaTime;
+
+            if (timeBetweenStepsCounter < 0) {
+                isMoving = true;
+                timeToMakeStepCounter = timeToMakeStep;
+                directionToMakeStep = new Vector2(Random.Range(-1,1), Random.Range(-1,1)) * speed;
+            }
+        }
+
+        animator.SetFloat(horizontal, directionToMakeStep.x);
+        animator.SetFloat(vertical, directionToMakeStep.y);
+        animator.SetBool(isMovingState, isMoving);
     }
 }

2acb1896e5e14ec2bcbbc4966803e1b1936916dc
Facundo <facundom.salazar@gmail.com>
2020-09-11 20:06:26 -0300

    MP igualado a HP

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,11 +8,18 @@ public class UIManager : MonoBehaviour
     public Slider playerHealthBar;
     public Text playerHealthText;
     public HealthManager playerHealthManeger;
+    public Slider playerMagicBar;
+    public Text playerMagicText;
+    public HealthManager playerMagicManeger;
+
 
     void Update()
     {
         playerHealthBar.maxValue = playerHealthManeger.maxHealth;
         playerHealthBar.value = playerHealthManeger.currentHealth;
         playerHealthText.text = "HP: " + playerHealthManeger.currentHealth + "/" + playerHealthManeger.maxHealth;
+        playerMagicBar.maxValue = playerMagicManeger.maxHealth;
+        playerMagicBar.value = playerMagicManeger.currentHealth;
+        playerMagicText.text = "HP: " + playerMagicManeger.currentHealth + "/" + playerMagicManeger.maxHealth;
     }
 }

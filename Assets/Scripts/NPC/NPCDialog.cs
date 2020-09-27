eaf86aa5f891410f2fcdca3bde48b5a58a094437
Nico <nmanograsso@gmail.com>
2020-09-27 18:46:59 -0300

    Add quest when talk to npcs

--- Assets/Scripts/NPC/NPCDialog.cs
+++ Assets/Scripts/NPC/NPCDialog.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class NPCDialog : MonoBehaviour {
     private DialogManager dialogManager;
     private NPCMovement npcMov;
+    private QuestManager questManager;    
 
     public bool playerInsideZone;
     public string[] dialog;
+    public bool hasDialogAfterQuestCompleted;
+    public int questID;
+    public string[] dialogWithQuestCompleted;
 
     void Start() {
         dialogManager = FindObjectOfType<DialogManager>();
         npcMov = gameObject.GetComponentInParent<NPCMovement>();
+        questManager = FindObjectOfType<QuestManager>();
     }
 
     void Update() {
         if(playerInsideZone && Input.GetKeyDown(KeyCode.Space) && !dialogManager.dialogActive) {
-            dialogManager.ShowDialog(dialog);
+            ShowDialog();
+
             if(npcMov)
                 npcMov.isTalking = true;
         }
@@ -34,5 +40,12 @@ public class NPCDialog : MonoBehaviour {
     private void OnTriggerExit2D(Collider2D other) {
         if(other.CompareTag("Player"))
             playerInsideZone = false;
+    }
+
+    void ShowDialog() {
+        if(hasDialogAfterQuestCompleted && questManager.questCompleted[questID])
+            dialogManager.ShowDialog(dialogWithQuestCompleted);
+        else
+            dialogManager.ShowDialog(dialog);
     }
 }
---
+++ Assets/Scripts/Quest/QuestNPC.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestNPC : MonoBehaviour {
+    private QuestManager questManager;
+    private NPCMovement npc;
+    private bool startQuestAfterTalking;
+
+    public int questID;
+    public bool isStartQuest;
+
+    void Start() {
+        questManager = FindObjectOfType<QuestManager>();
+        npc = gameObject.GetComponent<NPCMovement>();
+    }
+
+    void Update() {
+        if(npc.isTalking)
+            startQuestAfterTalking = true;
+        
+        if(startQuestAfterTalking && !npc.isTalking)
+            RunQuest();
+    }
+
+    void RunQuest() {
+        startQuestAfterTalking = false;
+         
+        if(isStartQuest && !questManager.quests[questID].gameObject.activeInHierarchy) {
+            questManager.quests[questID].gameObject.SetActive(true);
+            questManager.quests[questID].StartQuest();
+        }
+
+        if(!isStartQuest && questManager.quests[questID].gameObject.activeInHierarchy)
+            questManager.quests[questID].CompleteQuest();
+
+        GetComponent<QuestNPC>().enabled = false;
+    }
+}

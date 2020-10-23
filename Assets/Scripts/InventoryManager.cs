779184fa4c9a8a806fe246e20a1eab062a4dc5e6
Facundo <facundom.salazar@gmail.com>
2020-10-23 20:45:03 -0300

    Item y manejo de inventario
    Se empezó con eso xD

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour {
+    List <Item> items;
     Image image;
+    public void GetItem(Item newItem){
+        Debug.Log(newItem);
+        items.Add(newItem);
+    }
 
-    void Start() {
+    void Start() { 
         image = GetComponent<Image>();
+        items = new List<Item>();
     }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.I)){
             image.enabled = !image.enabled;
         }
+
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+    PolygonCollider2D polygonCollider2D;
+    public bool hasPolygonCollider2D;
+    void Start() {
+        if (hasPolygonCollider2D){
+             polygonCollider2D = GetComponent<PolygonCollider2D>();
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")){
+            FindObjectOfType<InventoryManager>().GetItem(this);
+        }
+    }
+}

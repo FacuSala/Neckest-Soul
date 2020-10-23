a74f193e5cdf63d59b32aeb0d1ee3eb93ca13729
Facundo <facundom.salazar@gmail.com>
2020-10-23 19:58:02 -0300

    empezamos con el invetnario

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour {
+    Image image;
+
+    void Start() {
+        image = GetComponent<Image>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.I)){
+            image.enabled = !image.enabled;
+        }
+    }
+}
